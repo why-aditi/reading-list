0e5469e6bc94c18a680be7ccf3abd4295277a9d8
Aditi Kala <130339327+why-aditi@users.noreply.github.com>
2024-07-06 13:50:55 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+async function fetchAndStoreBook(title) {
+  try {
+    const formattedTitle = title.toLowerCase().replace(/\s+/g, "+");
+
+    const response = await axios.get(
+      `https://openlibrary.org/search.json?q=${formattedTitle}`
+    );
+    const book = response.data.docs[0];
+    if (book) {
+      const id = book.cover_edition_key;
+      const title = book.title;
+      const author = book.author_name ? book.author_name[0] : "Unknown";
+      const author_id = book.author_key ? book.author_key[0] : null;
+      const edition = book.edition_key ? book.edition_key[0] : null;
+      const cover_id = book.cover_edition_key
+        ? book.cover_edition_key.toString()
+        : null;
+
+      await db.query(
+        "INSERT INTO books (id, title, author, author_id, edition, cover_id) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO NOTHING",
+        [id, title, author, author_id, edition, cover_id]
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching or inserting data:", error);
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM books ORDER BY id ASC");
+    res.render("index", { books: result.rows });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.post("/add-book", async (req, res) => {
+  const { title } = req.body;
+
+  try {
+    await fetchAndStoreBook(title);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding book");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
